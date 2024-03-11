1c43e6f410e059b8f8b737944d22aae384cfec4b
Michael <88445485+mjnyland@users.noreply.github.com>
2024-03-11 16:39:58 -0400

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Michael Nyland on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
